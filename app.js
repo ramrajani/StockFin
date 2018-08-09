2a7b3067f2b80071eae36e43715dc274860c9b50
ramrajani <ramrajani1998@gmail.com>
2018-08-09 13:48:28 +0530

    stocfin ejs and express added

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine","ejs");
+
+app.get("/",function(req,res){
+    res.render("index");
+})
+
+app.listen(3000,function(req,res){
+    console.log("server has started");
+})
