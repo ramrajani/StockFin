843a573c886467ed7847fe26698571356a173ecf
ramrajani <ramrajani1998@gmail.com>
2018-08-12 21:17:32 +0530

    updated

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set("view engine","ejs");
+app.use(express.static(__dirname + '/styles'));
 
 app.get("/",function(req,res){
     res.render("index");
