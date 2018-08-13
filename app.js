f9e018d9e63d90e1b9a529bb9071459cb358b04c
murtaza98 <demo.asd.asd>
2018-08-13 11:29:18 +0530

    login and sign up added

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ app.use(express.static(__dirname + '/styles'));
 
 app.get("/",function(req,res){
     res.render("index");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+})
+app.get("/signup",function(req,res){
+    res.render("signup");
 })
 
 app.listen(3000,function(req,res){
