0b38aa1cca2effadc10da214a687f85b6a79737e
Tushar Kadam <tushar.kadam@somaiya.edu>
2018-08-18 10:56:09 +0530

    Added key-Value for each row

--- JobDoer.java
+++ JobDoer.java
@@ -8,11 +8,13 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class JobDoer {
-    static  PrintWriter pw;
-    public static  void main(String args[]) throws Exception {
+    static PrintWriter pw;
+
+    public static void main(String args[]) throws Exception {
 
         readThenWriteJSON("C:\\Users\\Tushar\\IdeaProjects\\JavaCP\\src\\Practice\\stockjson1.json");
         pw.flush();
@@ -20,24 +22,25 @@ public class JobDoer {
 
 
     }
-    static void readThenWriteJSON(String name) throws Exception{
-            Object obj = new JSONParser().parse(new FileReader(name));
+
+    static void readThenWriteJSON(String name) throws Exception {
+        Object obj = new JSONParser().parse(new FileReader(name));
 
 
-        JSONObject jo=(JSONObject)obj;
+        JSONObject jo = (JSONObject) obj;
         JSONArray companyList = (JSONArray) jo.get("pusharray");
 
-        Iterator it=companyList.iterator();
-        while(it.hasNext()){
-            JSONObject companyObject=(JSONObject) it.next();
-            JSONObject editedCompany=new JSONObject();
-            editedCompany.put("name",companyObject.get("sharename"));
-            editedCompany.put("bse",companyObject.get("bse"));
-            editedCompany.put("nse",companyObject.get("nse"));
+        Iterator it = companyList.iterator();
+        while (it.hasNext()) {
+            JSONObject companyObject = (JSONObject) it.next();
+            JSONObject editedCompany = new JSONObject();
+            editedCompany.put("name", companyObject.get("sharename"));
+            editedCompany.put("bse", companyObject.get("bse"));
+            editedCompany.put("nse", companyObject.get("nse"));
 
 
-            String code=(String)companyObject.get("bse");
-            pw = new PrintWriter("C:\\Users\\Tushar\\IdeaProjects\\JavaCP\\src\\Practice\\output\\"+code+".json");
+            String code = (String) companyObject.get("bse");
+            pw = new PrintWriter("C:\\Users\\Tushar\\IdeaProjects\\JavaCP\\src\\Practice\\output\\" + code + ".json");
 
             addExcelData(editedCompany);
             pw.write(editedCompany.toJSONString());
@@ -49,46 +52,43 @@ public class JobDoer {
 
 
     }
-    static void addExcelData(JSONObject editedCompany) throws  Exception{
-    String bseCode=(String)editedCompany.get("bse");
-        File f=new File("C:\\Users\\Tushar\\IdeaProjects\\JavaCP\\src\\Practice\\stocks1\\"+bseCode+".xlsx");
-        FileInputStream fis=new FileInputStream(f);
+
+    static void addExcelData(JSONObject editedCompany) throws Exception {
+        String bseCode = (String) editedCompany.get("bse");
+        File f = new File("C:\\Users\\Tushar\\IdeaProjects\\JavaCP\\src\\Practice\\stocks1\\" + bseCode + ".xlsx");
+        FileInputStream fis = new FileInputStream(f);
         XSSFWorkbook wb = new XSSFWorkbook(fis);
-        XSSFSheet s1=wb.getSheetAt(5);
+        XSSFSheet s1 = wb.getSheetAt(5);
 
-    addData( editedCompany,s1, "ProfitLoss", 16, 31 );
-    addData( editedCompany,s1, "Quarters", 41, 50 );
-    addData( editedCompany,s1, "BalanceSheet", 56, 72 );
-    addData( editedCompany,s1, "CashFlow", 81, 85 );
-
-
-
-
-
-
-
-
-
-
-
+        addData(editedCompany, s1, "ProfitLoss", 16, 31);
+        addData(editedCompany, s1, "Quarters", 41, 50);
+        addData(editedCompany, s1, "BalanceSheet", 56, 72);
+        addData(editedCompany, s1, "CashFlow", 81, 85);
 
 
     }
-    static void  addData(JSONObject editedCompany,XSSFSheet s1,String parameter,int start,int end){
-        JSONObject year=new JSONObject();
-        int ind=1;
-        while(true) {
-            String date="";
+
+    static void addData(JSONObject editedCompany, XSSFSheet s1, String parameter, int start, int end) {
+        JSONObject year = new JSONObject();
+        int ind = 1;
+
+        ArrayList<String> al = new ArrayList<>();
+        for (int i = start; i < end; ++i) {
+            al.add(s1.getRow(i).getCell(0).toString());
+        }
+
+        while (true) {
+            String date = "";
             try {
-                date = s1.getRow(start-1).getCell(ind).toString();
-                int len=date.length();
-                date=date.substring(len-4,len);
-            }
-            catch (Exception e){
+                date = s1.getRow(start - 1).getCell(ind).toString();
+                int len = date.length();
+                date = date.substring(len - 4, len);
+            } catch (Exception e) {
                 break;
             }
-            JSONArray ja = new JSONArray();
-            for (int i = start; i < end; ++i) {
+            JSONObject keyValue = new JSONObject();
+
+            for (int i = start, cnt = 0; i < end; ++i, ++cnt) {
                 String element;
                 try {
                     element = s1.getRow(i).getCell(ind).toString();
@@ -96,21 +96,17 @@ public class JobDoer {
                 } catch (Exception e) {
                     element = "null";
                 }
-                ja.add(element);
+
+                keyValue.put(al.get(cnt), element);
 
             }
-            year.put(date, ja);
+            year.put(date, keyValue);
 
             ind++;
 
-
         }
-
-
         editedCompany.put(parameter, year);
 
-
     }
-
 
 }
