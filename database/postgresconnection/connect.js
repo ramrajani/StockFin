d5760c478179e3ecc6351dd0cb1a1c71ce510b7d
Ram Rajani <ramrajani1998@gmail.com>
2018-10-10 08:26:32 +0000

    added eps

--- database/postgresconnection/connect.js
+++ database/postgresconnection/connect.js
@@ -54,9 +54,32 @@ module.exports={
            query=""
        }else if(epsstart && epsend){
            query=""
+       }else if(salesstart && salesend){
+           query=""
+       }else if(facevaluestart && facevalueend){
+           query=""
+       }else if(equitystart && equityend){
+           query=""
        }
+    var today = new Date();
+    var yyyy = today.getFullYear();
+    var ypm1 = today.getFullYear()-1;
+    console.log(yyyy);
+    
+    
+    //query= 'SELECT p.eps,p.salea,p.netprofit,b.facevalue FROM profitloss'+ypm1+' p LEFT JOIN balancesheet'+ypm1+' b ON p.bse =b.bse WHERE p.bse= $1 '
        
-       
+        var query = {
+      text: 'SELECT p.bsecode  FROM profitloss'+ypm1+' p  WHERE p.eps BETWEEN   $1 and $2',
+      values:[epsstart,epsend]
+     
+    }
+
+    client.query(query, (err, resp) => {
+      console.log(err, resp)
+      console.log(resp.rows);
+       res.send(resp.rows);
+    })
        
        
    }
